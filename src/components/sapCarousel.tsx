c4754cd800ee8357b162a9f7226243f9cb25c661
Your Name <thaisna46@gmail.com>
2025-08-03 10:31:07 -0300

    ajustes

--- src/components/sapCarousel.tsx
+++ src/components/sapCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { twMerge } from "tailwind-merge";
 import { FiChevronLeft, FiChevronRight, FiPlay } from "react-icons/fi";
